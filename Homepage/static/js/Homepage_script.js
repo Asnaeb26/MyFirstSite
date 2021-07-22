c62c3d175a082d9a30150d57b6b95ee698cff9f5
Asnaeb26 <vp-11@mail.ru>
2021-07-23 01:31:05 +0300

    Добавил круглую диаграмму и сортировку

---
+++ Homepage/static/js/Homepage_script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+        $("#plus_button").click(function(){
+        $("#category").slideToggle("fast");
+        $(this).toggleClass("active");
+    })
+
+    $('#id45').click(function (e){
+        alert('Homepage tttttttttt')
+    })
+
+});
